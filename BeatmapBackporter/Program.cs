710c855322504a926ee7b00e723b146fd00c5d28
Kyuunex <kyuunex@protonmail.ch>
2020-10-12 15:00:10 +0400

    initial commit 2

---
+++ BeatmapBackporter/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BeatmapBackporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileLocation = string.Join(" ", args);
+            Console.WriteLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # BeatmapBackporter
 A tool to backport newer osu! beatmaps to be compatible in the 2008 version of osu!
+
+At this point in time, it's not usable
